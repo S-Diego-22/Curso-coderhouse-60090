360449bea441c225e33a36ea647c9e446119c48c
Diego Suasnavas <diegorpdg@gmailcom>
2024-11-20 18:49:57 -0300

    Agregue 2 funciones: sumar y restar

---
+++ Ejemplo_02.py
@@ -0,0 +1,4 @@
+def sumar(n1, n2):
+    return n1 + n2
+
+print(f"Resultado de la suma: {sumar(3,7)}")
---
+++ Ejemplo_03.py
@@ -0,0 +1,4 @@
+def restar(n1, n2):
+    return n1 - n2
+
+print(f"Resultado de la resta: {restar(7, 3)}")
